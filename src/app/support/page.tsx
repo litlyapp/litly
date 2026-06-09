4c92700e4ababe2e504cf1b3156ec706ee91e91e
Chad Knuth <chadknuth@Chad-Knuths-MacBook-Pro.local>
2026-06-09 15:52:25 +0900

    Add live Stripe payment links to support page

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 const STRIPE_LINKS = {
-  5: "https://donate.stripe.com/5kQ8wPd9h8UX8lNcxU0Ba01",
-  10: "https://donate.stripe.com/5kQ4gz7OXb35cC30Pc0Ba02",
-  25: "https://donate.stripe.com/8x2eVd6KT0or1XpeG20Ba03",
-  custom: "https://donate.stripe.com/fZu9ATfhp5ILfOfgOa0Ba04",
+  5: "https://donate.stripe.com/5kQ4gz7OXb35cC30Pc0Ba02",
+  10: "https://donate.stripe.com/8x2eVd6KT0or1XpeG20Ba03",
+  25: "https://donate.stripe.com/fZu9ATfhp5ILfOfgOa0Ba04",
+  custom: "https://donate.stripe.com/5kQ8wPd9h8UX8lNcxU0Ba01",
 };
 
 export const metadata = {
